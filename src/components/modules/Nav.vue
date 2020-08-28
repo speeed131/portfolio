943315242233c74231f58872d9c69cd7197140ea
Daiki Hirose <programer.sd131@gmail.com>
2020-08-28 10:43:25 +0900

    add activeclass

--- src/components/modules/Nav.vue
+++ src/components/modules/Nav.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
         <nav>
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
+            <router-link to="/" active-class="link--active" exact>Home</router-link>
+            <transition name="fade" mode="out-in">
+                <router-link to="/about" active-class="link--active" exact>About</router-link>
+            </transition>
+           
             <router-link to="/skills">Skills</router-link>
             <router-link to="/works">Works</router-link>
             <router-link to="/contact">Contact</router-link>
         </nav>
     </div>
 </template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active{
+        transition: opacity 0.5s;
+    }
+    .link--active{
+        font-size:20px;
+    }
+</style>
