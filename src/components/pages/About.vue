1a96e901023acb09eb0490c07377ca9972eb87cf
Daiki Hirose <programer.sd131@gmail.com>
2020-08-31 15:41:17 +0900

    creating

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,11 +1,34 @@
 <template>
-    <div>
-<font-awesome-icon :icon="['fab','github']"/>
+    <div class="about">
+<!-- <font-awesome-icon :icon="['fab','github']"/>
 <font-awesome-icon icon="globe" class="nav-icon"/>
-<font-awesome-icon icon="globe" class="nav-icon"/>
-
-
+<font-awesome-icon icon="user" class="nav-icon"/> -->
+        <h1>About</h1>
+        <div class="about-name">
+            <h2>Name</h2>
+            <p>廣瀬 大輝 (Daiki Hirose)</p>
+        </div>
+        <div class="about-school">
+            <h2>School</h2>
+            <p>AOYAMA GAKUIN UNIVERSITY</p>
+        </div>
+        <div class="about-birthday">
+            <h2>Birthday</h2>
+            <p>1997-5-30</p>
+        </div>
+        <div class="about-profile">
+            <h2>Profile</h2>
+            <p></p>
+        </div>
+    
+        <div class="about-twitter">
+            <h2>Twitter</h2>
+        </div>
+        <div class="about-github">
+            <h2>Github</h2>
+        </div>
+        <div class="about-qiita">
+            <h2>Qiita</h2>
+        </div>
     </div>
 </template>
-
-s
--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import VueParticles from 'vue-particles'
 
 //以下 font-awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGlobe,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
+import { faUser,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 //fontawesome用
-library.add(faGlobe,faBlog,faBirthdayCake,faImage,faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare)
+library.add(faUser,faBlog,faBirthdayCake,faImage,faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
