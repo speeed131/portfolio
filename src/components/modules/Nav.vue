21201eeb8a45612054e0b4b832feff2f177b7b0c
Daiki Hirose <programer.sd131@gmail.com>
2020-09-08 15:41:30 +0900

    create-nav-module close #42

---
+++ src/components/modules/Nav.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <nav class="nav">
+            <ul>
+                <li>
+                    <router-link to="/" active-class="link--active" exact>Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/about" active-class="link--active" exact>About</router-link>
+                </li>
+                <li>
+                    <router-link to="/skills" exact active-class="link--active">Skills</router-link>
+                </li>
+                <li>
+                    <router-link to="/works" exact active-class="link--active">Works</router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style lang="scss">
+    .nav {
+
+        > ul {
+            display: flex;
+            list-style: none;
+            align-items: center;
+
+            > li {
+                font-size: 3rem;
+                margin: 0 15px 0;
+                
+                > a {
+                    color: #ffffff;
+                }
+            }
+        }
+    }
+
+    .link--active {
+        font-size: 3.6rem;
+
+    }
+
+</style>
